b469f82d93b3f2ebd0bab051115e255777477b0b
Victor <x89168419357@gmail.com>
2017-07-27 19:13:53 +0300

    first commit

---
+++ circular-array.js
@@ -0,0 +1,41 @@
+﻿(function(factory) {
+    if (typeof window === 'undefined') {
+        module.exports = factory();
+    } else {
+        window.CircularArray = factory();
+    }
+}(function() {
+    function isIndex(value) {
+        return (value | 0) === parseInt(value);
+    }
+
+    function transformPropName(target, prop) {
+        if (isIndex(prop)) {
+            let len = target.length || 1;
+            prop = (prop | 0) % len;
+            if (prop < 0) {
+                prop += len;
+            }
+        }
+
+        return prop;
+    }
+
+    let proxyHandler = {
+        get(target, prop) {
+            if (prop === 'push') {
+                return (...args) => target.push(...args);
+            }
+
+            return target[transformPropName(target, prop)];
+        },
+        set(target, prop, value) {
+            target[transformPropName(target, prop)] = value;
+            return true;
+        }
+    };
+
+    return function(...args) {
+        return new Proxy(new Array(...args), proxyHandler);
+    };
+}));
